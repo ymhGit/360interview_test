6416708579818a89de280969a01be002bf811f12
ymh <yangminghaoymh@sina.com>
2015-09-04 17:17:28 +0800

    proxy_url

--- proxy_url/proxy_url/proxy_url_extractor.cc
+++ proxy_url/proxy_url/proxy_url_extractor.cc
@@ -6,28 +6,28 @@
 
 namespace qh
 {
-
+    
     namespace {
-
-        template< class _StringVector, 
+        
+        template< class _StringVector,
         class StringType,
-        class _DelimType> 
-            inline void StringSplit(  
-            const StringType& str, 
-            const _DelimType& delims, 
-            unsigned int maxSplits, 
-            _StringVector& ret)
+        class _DelimType>
+        inline void StringSplit(
+                                const StringType& str,
+                                const _DelimType& delims,
+                                unsigned int maxSplits,
+                                _StringVector& ret)
         {
             unsigned int numSplits = 0;
-
+            
             // Use STL methods
             size_t start, pos;
             start = 0;
-
+            
             do
             {
                 pos = str.find_first_of( delims, start );
-
+                
                 if ( pos == start )
                 {
                     ret.push_back(StringType());
@@ -48,18 +48,18 @@ namespace qh
                     *(ret.rbegin()) = StringType(str.data() + start, pos - start);
                     start = pos + 1;
                 }
-
+                
                 ++numSplits;
-
+                
             }
             while ( pos != StringType::npos );
         }
     }
-
+    
     ProxyURLExtractor::ProxyURLExtractor()
     {
     }
-
+    
     bool ProxyURLExtractor::Initialize( const std::string& param_keys_path )
     {
         std::ifstream ifs;
@@ -76,55 +76,187 @@ namespace qh
                 return false;
             }
             if (line.empty()) continue;
-
+            
             keysvect.clear();
             StringSplit(line, ",", static_cast<unsigned int>(-1), keysvect);
             assert(keysvect.size() >= 1);
             keys_set_.insert(keysvect.begin(), keysvect.end());
             keys_set_.erase("");
         }
-
+        
         ifs.close();
-
+        
         return true;
     }
-
+    
     std::string ProxyURLExtractor::Extract( const std::string& raw_url )
     {
         std::string sub_url;
         ProxyURLExtractor::Extract(keys_set_, raw_url, sub_url);
         return sub_url;
     }
-
+    
     void ProxyURLExtractor::Extract( const KeyItems& keys, const std::string& raw_url, std::string& sub_url )
     {
 #if 1
         //TODO 请面试者在这里添加自己的代码实现以完成所需功能
+        Tokener token(raw_url);
+        token.skipTo('?');
+        token.next(); //skip one char : '?'
+        std::string key;
+        while (!token.isEnd()) {
+            
+            //m_pCur-m_pData
+            u32 startPos=0;
+            //下一个&的位置，为0则表示后面没有&
+            u32 nextDividePos=0;
+            //下一个＝的位置，为0则表示后面没有＝
+            u32 nextEqualPos=0;
+            
+            bool isNull = false;
+            bool isHasEqual = false;
+            bool isHasDivide = false;
+            startPos = token.getCurPos();
+            
+            //查找下一个&的位置
+            if( token.skipTo('&')){
+                isHasDivide = true;
+                nextDividePos = token.getCurPos();
+                //回到起始
+                token.back(nextDividePos - startPos);
+            }
+                
+            //查找下一个＝的位置
+            if( token.skipTo('=')){
+                nextEqualPos = token.getCurPos();
+                
+                if (token.isEnd() ) {
+                    isNull = true;
+                }
+                
+                if (nextDividePos !=0 && nextEqualPos > nextDividePos) {
+                    isNull = true;
+                }
+                else {
+                    isHasEqual = true;
+                }
+                //回到起始
+                token.back(nextEqualPos - startPos );
+            }
+            
+            if (nextEqualPos + 1 == nextDividePos) {
+                isNull = true;
+            }
+            
+            if (isHasEqual && isHasDivide) {
+                key = token.nextString('=');
+                
+                if (keys.find(key) != keys.end()) {
+                    if( isNull){
+                        sub_url = "";
+                        return ;
+                    }
+                    const char* curpos = token.getCurReadPos();
+                    int nreadable = token.getReadableSize();
+                    
+                    /**
+                     * case 1:
+                     *  raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/&xx=yy"
+                     *  sub_url="http://hnujug.com/"
+                     */
+                    sub_url = token.nextString('&');
+                    
+                    // 遇到字符串末尾
+                    if (sub_url.empty() && nreadable > 0) {
+                        /**
+                         * case 2:
+                         * raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/"
+                         * sub_url="http://hnujug.com/"
+                         */
+                        assert(curpos);
+                        sub_url.assign(curpos, nreadable);
+                        return ;
+                    }else{
+                        return ;
+                    }
+                }
+                if( !token.skipTo('&'))
+                    return;
+                token.next();//skip one char : '&'
+            }
+            else if( !isHasEqual && isHasDivide){
+                key = token.nextString('&');
+                if (keys.find(key) != keys.end()) {
+                    sub_url = "";
+                    return ;
+                }
+            }else if( isHasEqual && !isHasDivide){
+                key=token.nextString('=');
+                if (keys.find(key) != keys.end()) {
+                if( isNull){
+                    sub_url = "";
+                    return ;
+                }
+                const char* curpos = token.getCurReadPos();
+                int nreadable = token.getReadableSize();
+                
+                /**
+                 * case 1:
+                 *  raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/&xx=yy"
+                 *  sub_url="http://hnujug.com/"
+                 */
+                sub_url = token.nextString('&');
+                
+                // 遇到字符串末尾
+                if (sub_url.empty() && nreadable > 0) {
+                    /**
+                     * case 2:
+                     * raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/"
+                     * sub_url="http://hnujug.com/"
+                     */
+                    assert(curpos);
+                    sub_url.assign(curpos, nreadable);
+                    return ;
+                }else{
+                    return ;
+                }
+                }
+                    return ;
+            }else{
+                key=token.nextString();
+                if (keys.find(key) != keys.end()) {
+                    sub_url = "";
+                    
+                }
+                return ;
+            }
+        }
+        
 #else
         //这是一份参考实现，但在特殊情况下工作不能符合预期
         Tokener token(raw_url);
         token.skipTo('?');
-        token.next(); //skip one char : '?' 
+        token.next(); //skip one char : '?'
         std::string key;
         while (!token.isEnd()) {
             key = token.nextString('=');
             if (keys.find(key) != keys.end()) {
                 const char* curpos = token.getCurReadPos();
                 int nreadable = token.getReadableSize();
-
+                
                 /**
-                * case 1: 
-                *  raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/&xx=yy"
-                *  sub_url="http://hnujug.com/"
-                */
+                 * case 1:
+                 *  raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/&xx=yy"
+                 *  sub_url="http://hnujug.com/"
+                 */
                 sub_url = token.nextString('&');
-
+                
                 if (sub_url.empty() && nreadable > 0) {
                     /**
-                    * case 2: 
-                    * raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/"
-                    * sub_url="http://hnujug.com/"
-                    */
+                     * case 2:
+                     * raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/"
+                     * sub_url="http://hnujug.com/"
+                     */
                     assert(curpos);
                     sub_url.assign(curpos, nreadable);
                 }
@@ -134,7 +266,7 @@ namespace qh
         }
 #endif
     }
-
+    
     std::string ProxyURLExtractor::Extract( const KeyItems& keys, const std::string& raw_url )
     {
         std::string sub_url;
